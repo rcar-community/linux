4d53682cc20447799ccba8388ad7a2ccad3d1f87
Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
2025-02-18 18:00:11 +0100

    media: rppx1: Add support for AWB measurement parameters and statistics
    Extend the RPPX1 driver to parse parameter blocks configuring for the
    auto white balance measurement window and parameters. As well as
    producing the measurements as part of a statistics buffer.
    
    This is the first ISP algorithm added to the RPPX1 driver and exercises
    both the parameter and statistics API provided by the base driver. It
    shows how the RkISP1 parameter and statistics buffer data can be scaled
    and adopted to fit the RPPX1 hardware.
    
    It also uses the parameter writing interface which allows the framework
    user to specify how (and when) the configuration are applied to the
    RPPX1.
    
    As the RkISP1 parameters and statics buffers have lower precision then
    the RPPX1 hardware the values needs to be scaled.
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>

--- drivers/media/platform/dreamchip/rppx1/rpp_stats.c
+++ drivers/media/platform/dreamchip/rppx1/rpp_stats.c
@@ -11,5 +11,9 @@ void rppx1_stats_fill_isr(struct rppx1 *rpp, u32 isc, void *buf)
 	struct rkisp1_stat_buffer *stats = buf;
 
 	stats->meas_type = 0;
+
+	if (isc & RPPX1_IRQ_ID_POST_AWB_MEAS)
+		if (!rpp_module_call(&rpp->post.wbmeas, stats_rkisp1, &stats->params))
+			stats->meas_type |= RKISP1_CIF_ISP_STAT_AWB;
 }
 EXPORT_SYMBOL_GPL(rppx1_stats_fill_isr);
