2ff66ce41d30305b783fba3a30864fc1025986e4
Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
2025-03-02 23:55:36 +0100

    media: rppx1: Add support for AWB gain settings
    Extend the RPPX1 driver to allow setting the AWB gains using the RkISP1
    parameter buffer format. This is the second function block inside the
    RPPX1 to be enabled and it uses the RPPX1 framework for parameters and
    its writer abstraction to allow the user to control how (and when)
    configuration is applied to the RPPX1.
    
    As the RkISP1 parameters buffer have lower precision then the RPPX1
    hardware the values needs to be scaled.
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>

--- drivers/media/platform/dreamchip/rppx1/rpp_params.c
+++ drivers/media/platform/dreamchip/rppx1/rpp_params.c
@@ -23,6 +23,9 @@ int rppx1_params_rkisp1(struct rppx1 *rpp, struct rkisp1_ext_params_cfg *cfg,
 		block_offset += block->header.size;
 
 		switch (block->header.type) {
+		case RKISP1_EXT_PARAMS_BLOCK_TYPE_AWB_GAIN:
+			module = &rpp->post.awbg;
+			break;
 		case RKISP1_EXT_PARAMS_BLOCK_TYPE_AWB_MEAS:
 			module = &rpp->post.wbmeas;
 			break;
