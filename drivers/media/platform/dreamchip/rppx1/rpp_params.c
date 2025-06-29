de8caaa83e0870ea31bc03c8a60d1008fb2d6cbd
Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
2025-06-29 16:18:40 +0200

    media: rppx1: Add support for Color Correction Matrix
    Extend the RPPX1 driver to allow setting the Color Correction Matrix
    (BLS) configuration using the RkISP1 parameter buffer format. It uses
    the RPPX1 framework for parameters and  its writer abstraction to allow
    the user to control how (and when) configuration is applied to the
    RPPX1.
    
    As the RkISP1 parameters buffer have lower precision then the RPPX1
    hardware the values needs to be scaled. The behavior matches the RkISP1
    hardware.
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>

--- drivers/media/platform/dreamchip/rppx1/rpp_params.c
+++ drivers/media/platform/dreamchip/rppx1/rpp_params.c
@@ -29,6 +29,9 @@ int rppx1_params_rkisp1(struct rppx1 *rpp, struct rkisp1_ext_params_cfg *cfg,
 		case RKISP1_EXT_PARAMS_BLOCK_TYPE_AWB_GAIN:
 			module = &rpp->post.awbg;
 			break;
+		case RKISP1_EXT_PARAMS_BLOCK_TYPE_CTK:
+			module = &rpp->post.ccor;
+			break;
 		case RKISP1_EXT_PARAMS_BLOCK_TYPE_AWB_MEAS:
 			module = &rpp->post.wbmeas;
 			break;
