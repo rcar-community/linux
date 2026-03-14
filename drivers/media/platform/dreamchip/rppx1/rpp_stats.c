e1194ecef3bf49bb9b99b86616c5c40bae855397
Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
2026-03-14 22:59:37 +0100

    media: rppx1: Add support for Auto Exposure Measurement
    Extend the RPPX1 driver to allow setting the EXM configuration using the
    RkISP1 parameter buffer format. It uses the RPPX1 framework for
    parameters and its writer abstraction to allow the user to control how
    (and when) configuration is applied to the RPPX1.
    
    As the RkISP1 parameters buffer have lower precision then the RPPX1
    hardware the values needs to be scaled. The behavior matches the RkISP1
    hardware.
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    Tested-by: Marek Vasut <marek.vasut+renesas@mailbox.org>

--- drivers/media/platform/dreamchip/rppx1/rpp_stats.c
+++ drivers/media/platform/dreamchip/rppx1/rpp_stats.c
@@ -15,5 +15,9 @@ void rppx1_stats_fill_isr(struct rppx1 *rpp, u32 isc, void *buf)
 	if (isc & RPPX1_IRQ_ID_POST_AWB_MEAS)
 		if (!rpp_module_call(&rpp->post.wbmeas, stats_rkisp1, &stats->params))
 			stats->meas_type |= RKISP1_CIF_ISP_STAT_AWB;
+
+	if (isc & RPPX1_IRQ_ID_PRE1_EXM)
+		if (!rpp_module_call(&rpp->pre1.exm, stats_rkisp1, &stats->params))
+			stats->meas_type |= RKISP1_CIF_ISP_STAT_AUTOEXP;
 }
 EXPORT_SYMBOL_GPL(rppx1_stats_fill_isr);
