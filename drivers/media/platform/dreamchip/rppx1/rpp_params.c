6b138ec8ab493df7e893cff5ce2a2acfe6b9b9b1
Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
2026-03-14 22:59:41 +0100

    media: rppx1: Add support for Lens Shade Correction
    Extend the RPPX1 driver to allow setting the Lens Shade Correction (LSC)
    configuration using the RkISP1 parameter buffer format. It uses the
    RPPX1 framework for parameters and  its writer abstraction to allow the
    user to control how (and when) configuration is applied to the RPPX1.
    
    As the RkISP1 parameters buffer only describes one quadrant of the lens,
    this is due to the RkISP1 hardware only allowing one quadrant to be
    programmed to hardware. The hardware then extrapolates this to the other
    three quadrants of the lens. For RPP all four quadrants are individually
    programmable.
    
    To compensate for the driver need to extrapolate all four quadrants from
    the RkISP1 parameters buffer information.
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    Tested-by: Marek Vasut <marek.vasut+renesas@mailbox.org>

--- drivers/media/platform/dreamchip/rppx1/rpp_params.c
+++ drivers/media/platform/dreamchip/rppx1/rpp_params.c
@@ -19,6 +19,7 @@ rkisp1_ext_params_blocks_info[] = {
 	RKISP1_PARAMS_BLOCK_INFO(BLS, bls),
 	RKISP1_PARAMS_BLOCK_INFO(AWB_GAIN, awb_gain),
 	RKISP1_PARAMS_BLOCK_INFO(CTK, ctk),
+	RKISP1_PARAMS_BLOCK_INFO(LSC, lsc),
 	RKISP1_PARAMS_BLOCK_INFO(AWB_MEAS, awb_meas),
 	RKISP1_PARAMS_BLOCK_INFO(HST_MEAS, hst),
 	RKISP1_PARAMS_BLOCK_INFO(AEC_MEAS, aec),
@@ -64,6 +65,9 @@ int rppx1_params(struct rppx1 *rpp, struct vb2_buffer *vb, size_t max_size,
 			break;
 		case RKISP1_EXT_PARAMS_BLOCK_TYPE_CTK:
 			module = &rpp->post.ccor;
+			break;
+		case RKISP1_EXT_PARAMS_BLOCK_TYPE_LSC:
+			module = &rpp->pre1.lsc;
 			break;
 		case RKISP1_EXT_PARAMS_BLOCK_TYPE_AWB_MEAS:
 			module = &rpp->post.wbmeas;
