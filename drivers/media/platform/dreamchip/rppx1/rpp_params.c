da5cf8aa9f36b31d176f420e5a8b625c517c7c1b
Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
2026-03-14 22:59:44 +0100

    media: rppx1: Add support for Bilateral Denoising
    Extend the RPPX1 driver to allow setting the Bilateral Denoising
    configuration using the RkISP1 parameter buffer format. It uses the
    RPPX1 framework for parameters and its writer abstraction to allow the
    user to control how (and when) configuration is applied to the RPPX1.
    
    The parameter bit-sizes matches RkISP1 so there is no need to convert
    between the two. Some bit flags are inverted however and RPP have
    different registers for each color components coefficients where RkISP1
    have one for covering all.
    
    The biggest difference is that RPP have dropped the hardware bit
    AWB_GAIN_COMP. Luckily it's behavior is easy to emulate in software.
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    Tested-by: Marek Vasut <marek.vasut+renesas@mailbox.org>

--- drivers/media/platform/dreamchip/rppx1/rpp_params.c
+++ drivers/media/platform/dreamchip/rppx1/rpp_params.c
@@ -22,6 +22,8 @@ rkisp1_ext_params_blocks_info[] = {
 	RKISP1_PARAMS_BLOCK_INFO(BDM, bdm),
 	RKISP1_PARAMS_BLOCK_INFO(CTK, ctk),
 	RKISP1_PARAMS_BLOCK_INFO(GOC, goc),
+	RKISP1_PARAMS_BLOCK_INFO(DPF, dpf),
+	RKISP1_PARAMS_BLOCK_INFO(DPF_STRENGTH, dpf_strength),
 	RKISP1_PARAMS_BLOCK_INFO(LSC, lsc),
 	RKISP1_PARAMS_BLOCK_INFO(AWB_MEAS, awb_meas),
 	RKISP1_PARAMS_BLOCK_INFO(HST_MEAS, hst),
@@ -76,6 +78,11 @@ int rppx1_params(struct rppx1 *rpp, struct vb2_buffer *vb, size_t max_size,
 			break;
 		case RKISP1_EXT_PARAMS_BLOCK_TYPE_GOC:
 			module = &rpp->hv.ga;
+			break;
+		case RKISP1_EXT_PARAMS_BLOCK_TYPE_DPF:
+		case RKISP1_EXT_PARAMS_BLOCK_TYPE_DPF_STRENGTH:
+			/* Both types handled by the same block. */
+			module = &rpp->pre1.bd;
 			break;
 		case RKISP1_EXT_PARAMS_BLOCK_TYPE_LSC:
 			module = &rpp->pre1.lsc;
